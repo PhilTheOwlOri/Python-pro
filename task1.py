43b9bccf4de0757de4d927dcffab3a10416e7cfc
PhilTheOwlOri <2303@lfp.edu.co>
2026-06-17 19:16:30 -0400

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+None
+meme_dict = {
+    "LULZ:es una variante de LOL como decirlo de una forma Silly",
+    "SILLY:significa tonto, bobo o absurdo, y se usa para describir algo sin sentido, trivial o divertido pero de poca importancia.",
+}
+word = input("Escribe una palabra que no entiendes y con masyusculas!: ")
+
+if word in meme_dict.keys():
+    .
+else:
+    print("no existe ninguna palabra como esta sorry")
